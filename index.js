4944ec31e9b60b7a399768f582e46b489158c0d0
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-12 17:00:46 +0000

    Add db and resolvers

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import { ApolloServer } from "@apollo/server";
-import {startStandaloneServer} from "@apollo/server/standalone";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 const server = new ApolloServer({
-
+  typeDefs,
+  resolvers
 });
 
-const {url} = startStandaloneServer(server, {
-    listen: {
-        port: 4000
-    }
+const { url } = startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
 });
 
 console.log("Server listening at port", 4000);
--- schema.js
+++ schema.js
@@ -1,3 +1,25 @@
-export const typeDefs = ` #graphql
-    type 
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platforms: [String!]!
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+
+    type Review {
+        id: ID!
+        content: String!
+        rating: String!
+    }
+
+    type Query {
+        reviews: [Review]
+        authors: [Author]
+        games: [Game]
+    }
 `;
