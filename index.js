9f8aeb1abae7b6e28a00526299380500c95877a4
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-13 17:02:01 +0000

    Create nested queries and delete mutation

--- index.js
+++ index.js
@@ -8,13 +8,45 @@ const resolvers = {
     games() {
       return db.games;
     },
+    game(_, args) {
+      return db.games.find(game => game.id === args.id);
+    },
     authors() {
       return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find(author => author.id === args.id);
     },
     reviews() {
       return db.reviews;
     },
+    review(_, args) {
+      return db.reviews.find(review => review.id === args.id);
+    }
   },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter(review => review.game_id === parent.id);
+    }
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter(review => review.author_id === parent.id)
+    }
+  },
+  Review: {
+    game(parent) {
+      return db.games.find(game => game.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find(author => author.id === parent.author_id);
+    }
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      return db.games.filter(game => game.id !== args.id);
+    }
+  }
 };
 
 const server = new ApolloServer({
--- schema.js
+++ schema.js
@@ -3,24 +3,36 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platforms: [String!]!
+        reviews: [Review!]
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
     type Review {
         id: ID!
         content: String!
         rating: String!
+        game: Game!
+        author: Author!
     }
 
     type Query {
         reviews: [Review]
         review(id: ID!): Review
         authors: [Author]
+        author(id: ID!): Author
         games: [Game]
+        game(id: ID!): Game
     }
+
+    type Mutation {
+        deleteGame(id: ID!): [Game]
+    }
+
+    
 `;
